3c3994d635bd6983a59853490beb20297015deec
Shuhrat <abbosovich1999@gmail.com>
2025-11-24 23:08:49 +0500

    vaa

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-const jsonServer = require("json-server");
 const express = require("express");
+const jsonServer = require("json-server");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-
 server.use((req, res, next) => {
-  setTimeout(next, 500);
+  setTimeout(next, 500); // Slow latency
 });
+
 setTimeout(() => {
   server.use(router);
-  app.use(server);
 }, 500);
 
-const app = express();
+app.use(server);
 
-const PORT = 3600;
+const PORT = process.env.PORT || 3600;
+
 app.listen(PORT, () => {
-  console.log("Server is running on... http://localhost:3600");
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-// /
